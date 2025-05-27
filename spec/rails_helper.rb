8c408559f4409135738cd411ec9b1c2c139ebeaa
Jukka Heikkinen <remiraz@gmail.com>
2025-05-27 14:24:47 +0300

    D2.0

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,10 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # return unless Rails.env.test?
 require 'rspec/rails'
 require 'factory_bot_rails'
+
+# Explicitly require rails-controller-testing
+require 'rails/controller/testing'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
